20e757f59c6639eae0acec22f18e4fc4db189a41
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 15:16:05 +0200

    Added Report_ShouldShowOpeningCount test

--- SmartDevices.Test/DoorSensorTests.cs
+++ SmartDevices.Test/DoorSensorTests.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using SmartDevices.Devices;
 namespace SmartDevices.Test
 {
-    internal class DoorSensorTests
+    public  class DoorSensorTests
     {
-    }
+        [Fact]
+
+        public void NewDoorSensor_ShouldBeClosed() 
+        
+        {
+            //Arrange
+            DoorSensor door = new DoorSensor("Front Door");
+
+            //Act 
+            bool result = door.IsOpen;
+
+            //Assert 
+            Assert.False(result);
+
+        
+        }
+        [Fact]
+        public void RecordOpening_ShouldOpenDoor()
+        {
+            // Arrange
+            DoorSensor door = new DoorSensor("Front Door");
+
+            //Act
+            door.RecordOpening();
+
+            //Assert
+            Assert.True(door.IsOpen);
+        
+        }
+
+        [Fact]
+        public void Report_ShouldShowOpeningCount()
+        {
+            //Arrange
+            DoorSensor door = new DoorSensor("Front Door");
+
+            //Act
+            door.RecordOpening();
+            door.RecordOpening();
+            door.RecordOpening();
+
+            string report = door.Report();
+
+            //Assert
+            Assert.Contains("opened 3 times today",report);
+
+        }
+    }// End of class
 }
