ce8533979b1a4c85c0213a919dd45c4c4479e08f
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 11:29:52 +0200

    Created 2 Unit tests NewLight_ShouldBeOff and TurnOn_ShouldTurnLightOn

---
+++ SmartDevices.Test/LightTests.cs
@@ -0,0 +1,44 @@
+﻿using SmartDevices.Devices;
+
+namespace SmartDevices.Test
+{
+    public class LightTests
+    {
+        [Fact]
+        public void NewLight_ShouldBeOff()
+        {
+
+            //Arrange
+
+            Light light = new Light("Kitchen Light");
+
+            //Act
+            bool result = light.IsOn;
+
+            //Assert 
+            Assert.False(result);
+
+        }
+
+
+        [Fact]
+
+        public void TurnOn_ShouldTurnLightOn()
+       
+        {
+            // Arrange
+
+            Light light = new Light("Kitchen Light");
+
+            //Act
+            light.TurnOn();
+
+            //Assert
+            Assert.True(light.IsOn);
+            Assert.Equal("On", light.Status);
+        
+        }
+
+
+    }//Ending of class
+}
