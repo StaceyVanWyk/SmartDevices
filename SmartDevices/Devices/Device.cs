abd335d310344fb63e39e75b80070f3f89f720a3
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 09:25:28 +0200

    Created SmartDevice class

---
+++ SmartDevices/Devices/Device.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartDevices.Devices
+{
+    public abstract class Device
+    {
+        public string Name { get;  }
+
+        public Device(string name) {
+
+            Name = name;
+        }
+
+        public string Status {
+
+            get;
+            protected set;
+        
+        }
+
+
+        public abstract string Report();
+    }
+}
---
+++ SmartDevices/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartDevices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
