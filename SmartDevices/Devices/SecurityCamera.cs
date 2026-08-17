ddedf561453aa963a59e58c4faf4f8d7969f3698
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 13:33:56 +0200

    Impleted constructor, add the wattage and add the report string in SecurityCamera class

--- SmartDevices/Devices/SecurityCamera.cs
+++ SmartDevices/Devices/SecurityCamera.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace SmartDevices.Devices
 {
-    internal class SecurityCamera
+    public  class SecurityCamera : SwitchableDevice
     {
-    }
+
+        public SecurityCamera(string name) : base(name) 
+        
+        {
+        
+        }
+
+        public override double RatedWatts => 12;
+
+
+        public override string Report()
+        {
+            return $"{Name} | {Status} | {RatedWatts}W | {TotalKilowattHours:F3} kWh";
+        }
+    }// End of Class
 }
