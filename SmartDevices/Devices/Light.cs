6906a9fab5561ae9bad981487b1c9a86e2967c30
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 10:56:32 +0200

    Add Light device

---
+++ SmartDevices/Devices/Light.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartDevices.Devices
+{
+    public class Light : SwitchableDevice
+    {
+
+        public Light (string name ) : base (name)
+        
+        {
+        
+        }
+
+        public override double RatedWatts => 9;
+
+        public override string Report() {
+
+            return $"{Name} | {Status} | {RatedWatts}W | {TotalKilowattHours:F3} KWh";
+
+
+        }
+
+
+    }// end of class
+
+    
+}
