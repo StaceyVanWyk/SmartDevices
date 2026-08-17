3bea4eb2fcc1a103fdf3758cd7cc60c2e58af3d6
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 15:47:38 +0200

    Created a list and added all the devices

--- SmartDevices/Program.cs
+++ SmartDevices/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SmartDevices
+﻿using SmartDevices.Devices;
+
+namespace SmartDevices
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Hello, World!");
+
+            //Creating a list
+            List<Device> devices = new List<Device>();
+
+            Light light = new Light("Kitchen Light");
+            Kettle kettle = new Kettle("Kettle");
+            SecurityCamera camera = new SecurityCamera("Front camera");
+            Thermostat thermostat = new Thermostat("Themostat");
+            DoorSensor door = new DoorSensor("Front Door");
+
+            devices.Add(light);
+            devices.Add(kettle);
+            devices.Add(camera);
+            devices.Add(thermostat);
+            devices.Add(door);
+
+            light.TurnOn();
+            camera.TurnOn();
+
         }
     }
 }
