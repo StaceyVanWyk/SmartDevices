4c1146017076d8078b80c50344452e483ee23289
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 13:19:25 +0200

    Added unit test Kettle_ShouldHave2200Watts

--- SmartDevices.Test/KettleTests.cs
+++ SmartDevices.Test/KettleTests.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SmartDevices.Devices;
 
 namespace SmartDevices.Test
 {
-    internal class KettleTests
+    public class KettleTests
     {
-    }
+        [Fact]
+
+        public void Kettle_ShouldHave2200Watts() 
+        
+        {
+            //Arrange
+
+            Kettle kettle = new Kettle("Kitchen Kettle");
+
+            // Act 
+            double watts = kettle.RatedWatts;
+
+            //Assert
+            Assert.Equal(2200, watts);       
+        }g
+    }// End of class
 }
