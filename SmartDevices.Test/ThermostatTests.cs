5ea51883a745e117370583c05b104e25ef8480ca
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 14:21:50 +0200

    Add funtionality to ThermostatTests

--- SmartDevices.Test/ThermostatTests.cs
+++ SmartDevices.Test/ThermostatTests.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SmartDevices.Devices;
 
 namespace SmartDevices.Test
 {
     public class ThermostatTests
     {
-    }
+
+        [Fact]
+        public void SetTargetTemperatur_ShouldChangeTarget() 
+        {
+            // Arrange
+            Thermostat thermostat = new Thermostat("Living Room Thernostat");
+
+            // Act
+            thermostat.SetTargetTemperature(21);
+
+            // Assert
+            Assert.Equal(21, thermostat.TargetTemperature);
+        
+        }
+
+
+    }// End of Class
 }
