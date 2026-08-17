465d973c1552670bb549ef9a602300eb458af694
Stacey Van Wyk <svanwyk722@gmail.com>
2026-08-17 13:41:18 +0200

    Added SecurityCamera_ShouldHave12Watts test

--- SmartDevices.Test/SecurityCameraTests.cs
+++ SmartDevices.Test/SecurityCameraTests.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SmartDevices.Devices;
 
 namespace SmartDevices.Test
 {
-    internal class SecurityCameraTests
+    public  class SecurityCameraTests
     {
-    }
+
+        [Fact]
+        public void SecurityCamera_ShouldHave12Watts()
+        {
+            //Arrange 
+            SecurityCamera camera = new SecurityCamera("Front Camera");
+
+            // Act
+            double watts = camera.RatedWatts;
+
+            //Assert
+            Assert.Equal(12, watts);
+        }
+    }// End of class
 }
